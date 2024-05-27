d126675cc02470c2533b25e83100db42a8dedc38
Mohankumar@LAPTOP-DSRRQ3HB <gmk9155@gmail.com>
2024-05-27 20:04:48 +0530

    first commit

---
+++ src/component/Divider.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../CSS/Divider.css'
+
+const Divider = () => {
+  return (
+    <div className='Divider-container'> 
+    </div>
+  )
+}
+
+export default Divider;
